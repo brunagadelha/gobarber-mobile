972b6e42bf509101fa05bffee83852c346b96f4f
MicroPower - Bruna Gadelha de Souza Silva <bruna.silva@micropower.com.br>
2020-08-20 19:41:40 -0300

    structure and standards

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View
-      style={{
-        backgroundColor: '#312e38',
-        flex: 1,
-        alignItems: 'center',
-      }}
-    >
-      <Text style={{ color: '#FFF', flex: 1 }}>Hello World</Text>
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Routes />
     </View>
-  </>
+  </NavigationContainer>
 );
 
 export default App;
